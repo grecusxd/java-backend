aaa7666eea92186ac3a92826236791294fa7a6ca
Grecusxd <douglas.gomes41@gmail.com>
2023-10-21 18:57:25 -0300

    alteração

--- iphone/iphone/src/Iphone.java
+++ iphone/iphone/src/Iphone.java
@@ -5,28 +5,29 @@ public class Iphone {
         escolhaApp();
     }
     public static void escolhaApp(){
-        Scanner scanner=new Scanner(System.in);
-        Navegador abrirnav=new Navegador();
-        Telefone telefone=new Telefone();
-        ReprodutorMusical ipod=new ReprodutorMusical();
-        System.out.println("1. Ipod | 2. Telefone | 3. Safari");        
-        String opt=scanner.nextLine();
-        switch(opt){
-            case "1":
-                System.out.println("Abrindo Ipod");
-                ipod.musicEsacolha();
-                break;
-            case "2":
-                System.out.println("Abrindo Telefone");
-                telefone.telOpt();
-                break;
-            case "3":
-                System.out.println("Abrindo Safari");
-                abrirnav.navEsacolha();
-                break;
-            default:
-                System.out.println("Opção invalida");
-                break;
-    }
+        try (Scanner scanner = new Scanner(System.in)) {
+            Navegador abrirnav=new Navegador();
+            Telefone telefone=new Telefone();
+            ReprodutorMusical ipod=new ReprodutorMusical();
+            System.out.println("1. Ipod | 2. Telefone | 3. Safari");        
+            String opt=scanner.nextLine();
+            switch(opt){
+                case "1":
+                    System.out.println("Abrindo Ipod");
+                    ipod.musicEsacolha();
+                    break;
+                case "2":
+                    System.out.println("Abrindo Telefone");
+                    telefone.telOpt();
+                    break;
+                case "3":
+                    System.out.println("Abrindo Safari");
+                    abrirnav.navEsacolha();
+                    break;
+                default:
+                    System.out.println("Opção invalida");
+                    break;
+   }
+        }
 }
 }
